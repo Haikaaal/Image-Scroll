1b73b7e72b3876fe10cf6639d8572e1e2c23eb0f
Haikaaal <haikal.athallarik@gmail.com>
2025-06-11 13:11:36 +0700

    change color and add image

--- app/src/main/java/com/example/affirmations/ui/theme/Color.kt
+++ app/src/main/java/com/example/affirmations/ui/theme/Color.kt
@@ -17,9 +17,9 @@ package com.example.affirmations.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val md_theme_light_background = Color(0xFFFBFCFE)
+val md_theme_light_background = Color(0xFFC8D5B9)
 val md_theme_light_surfaceVariant = Color(0xFFE7E0EC)
-val md_theme_light_onSurfaceVariant = Color(0xFF49454f)
+val md_theme_light_onSurfaceVariant = Color(0xFFFAF3DD)
 
 val md_theme_dark_background = Color(0xFF191C1D)
 val md_theme_dark_surfaceVariant = Color(0xFF49454f)
